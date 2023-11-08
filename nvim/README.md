7930036c5024c4054db61501b2320f820a120baf
ShanRB <shanrb119@gmail.com>
2023-11-08 22:14:07 +1100

    add session management

--- nvim/README.md
+++ nvim/README.md
@@ -30,12 +30,12 @@ In the following shortcuts, `<leader>` represents `space`.<br>
 - "folke/zen-mode.nvim"
 - "preservim/vim-markdown" and "iamcco/markdown-preview.nvim"
 - "stevearc/aerial.nvim"
-- "folke/persistence.nvim"
 - smarter yank and clipboard
 - config nvim-notify
 
 ### possible improvements
 - more telescope plugins? and pickers?
+- "stevearc/aerial.nvim"?
 - "simrat39/symbols-outline.nvim" is quite useful but can lspsaga do the same or not?
 - "joechrisellis/lsp-format-modifications.nvim" is useful but needs to be added to lsp keymap after it's cleaned
 
--- nvim/lua/nvshan/plugins/editor/init.lua
+++ nvim/lua/nvshan/plugins/editor/init.lua
@@ -69,6 +69,17 @@ local plugins = {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = vim.opt.sessionoptions:get() },
+    -- stylua: ignore
+    keys = {
+      { "<leader>ss", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>sr", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>sq", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
 }
 
 return plugins
