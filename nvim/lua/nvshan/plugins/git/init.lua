bb352b4e66059fb1fa1b29f0882f8119549fd2f9
ShanRB <shanrb119@gmail.com>
2023-12-11 01:44:58 +1100

    adjust keymap for git

--- nvim/lua/nvshan/plugins/git/init.lua
+++ nvim/lua/nvshan/plugins/git/init.lua
@@ -21,25 +21,5 @@ return {
     config = function()
       require("nvshan.plugins.git.gitsigns")
     end,
-    keys = {
-      -- Navigation
-      { "]h", function() require("gitsigns").next_hunk() end, desc = "Git Next Hunk" },
-      { "[h", function() require("gitsigns").prev_hunk() end, desc = "Git Previous Hunk" },
-      { "gn", function() require("gitsigns").next_hunk() end, desc = "Git Next Hunk ([h)" },
-      { "gp", function() require("gitsigns").prev_hunk() end, desc = "Git Previous Hunk ([h)" },
-      -- Actions
-      { "<leader>gs", function() require("gitsigns").stage_hunk() end, mode = { "n", "v" }, desc = "Git Stage Hunk" },
-      { "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, desc = "Git Unstage Hunk" },
-      { "<leader>gr", function() require("gitsigns").reset_hunk() end, mode = { "n", "v" }, desc = "Git Reset Hunk" },
-      { "<leader>gS", function() require("gitsigns").stage_buffer() end, desc = "Git Stage Buffer" },
-      { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Git Reset Buffer" },
-      { "<leader>gb", function() require("gitsigns").toggle_current_line_blame() end, desc = "Git Blame (Current Line)" },
-      { "<leader>gB", function() require("gitsigns").blame_line({full=true}) end, desc = "Git Blame" },
-      { "<leader>gd", function() require("gitsigns").diffthis() end, desc = "Git Diff" },
-      { "<leader>gD", function() require("gitsigns").diffthis('~') end, desc = "Git Diff All" },
-      { "<leader>gU", function() require("gitsigns").toggle_deleted() end, desc = "Git Toggle Deleted" },
-      -- Text Object
-      { "ih", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" }, desc = "Select in hunk"},
-    }
   },
 }
