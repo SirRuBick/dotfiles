88cd9406fb08ef985301a3e5d79c2c01575dde49
SirRuBick <shanrb119@gmail.com>
2023-12-16 23:02:03 +1100

    diable auto change dir in nvim project

--- nvim/lua/nvshan/plugins/editor/project.lua
+++ nvim/lua/nvshan/plugins/editor/project.lua
@@ -5,13 +5,13 @@ local project = require("project_nvim")
 project.setup({
   -- Manual mode doesn't automatically change your root directory, so you have
   -- the option to manually do so using `:ProjectRoot` command.
-  manual_mode = false,
+  manual_mode = true,
 
   -- Methods of detecting the root directory. **"lsp"** uses the native neovim
   -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
   -- order matters: if one is not detected, the other is used as fallback. You
   -- can also delete or rearangne the detection methods.
-  detection_methods = { "lsp", "pattern" },
+  detection_methods = { "pattern", "lsp" },
 
   -- All the patterns used to detect root dir, when **"pattern"** is in
   -- detection_methods
@@ -30,13 +30,13 @@ project.setup({
 
   -- When set to false, you will get a message when project.nvim changes your
   -- directory.
-  silent_chdir = true,
+  silent_chdir = false,
 
   -- What scope to change the directory, valid options are
   -- * global (default)
   -- * tab
   -- * win
-  scope_chdir = 'global',
+  scope_chdir = "win",
 
   -- Path where project.nvim will store the project history for use in
   -- telescope
