45341b9a253918107f9e82b5683736cada742525
alex.shan <shanrb119@gmail.com>
2026-01-26 22:37:39 +0800

    add powershell

---
+++ powershell/profile.ps1
@@ -0,0 +1,80 @@
+# ####################
+# Powershell Profile
+# by: Alex Shan
+# ####################
+
+# General Settings
+## UTF8
+try {
+    [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+    chcp 65001 > $null
+} catch {}
+
+Clear-Host
+
+# Alias
+Set-Alias tt tree
+Set-Alias ls Get-ChildItem
+Set-Alias ll Get-ChildItem -Force
+Set-Alias grep Select-String
+Set-Alias cat Get-Content
+Set-Alias clear Clear-Host
+Set-Alias rm Remove-Item
+Set-Alias touch New-Item
+Set-Alias reboot Restart-Computer
+Set-Alias vim nvim
+Set-Alias v nvim
+Set-Alias ff fastfetch
+Set-Alias lg lazygit
+
+
+# Evnrionment Variables
+
+
+# Prompt
+## FastFetch
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    fastfetch -c "$env:USERPROFILE/.config/fastfetch/config.jsonc"
+} else {
+    Write-Host "FastFetch not found. Install it with: winget install fastfetch" -ForegroundColor Yellow
+}
+## Starship
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+} else {
+    Write-Host "Starship not found. Install it with: winget install starship" -ForegroundColor Yellow
+}
+## Zoxide
+# winget install ajeetdsouza.zoxide
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+} else {
+    Write-Host "Zoxide not found. Install it with: winget install ajeetdsouza.zoxide" -ForegroundColor Yellow
+}
+## FZF
+# winget install fzf
+
+
+# Modules
+# Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+Import-Module Terminal-Icons
+# Install-Module -Name z -Force
+# Import-Module 
+# Install-Module -Name PSReadLine -Scope CurrentUser -Force
+# PSReadLine
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+
+# Functions
+## whereis
+function whereis ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+  Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+  ## Quick open recycle bin
+function openRecycleBin(){
+  start shell:RecycleBinFolder
+}
